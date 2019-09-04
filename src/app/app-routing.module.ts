cab1026419becd742c4e2f56651f7823d97d01f8
Samson.Buffer <samson.buffer@BUFFER-Ayobami.local>
2019-09-04 16:29:57 +0100

    initial and blank ui

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomepageComponent} from "./homepage/homepage.component";
+import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : '',
+    component : HomepageComponent,
+    pathMatch : 'full'
+  },
+
+  {
+    path: '**',
+    component: PageNotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
