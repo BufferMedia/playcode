7ce396ee1a4c4c17fe97251ae0fb293d689ee2af
samtax01 <samsoniyanu@hotmail.com>
2019-10-22 10:33:52 +0100

    22/10/2019 fresh commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,62 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {HomepageComponent} from "./homepage/homepage.component";
-import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
+import {HomepageComponent} from './pages/homepage/homepage.component';
+import {PageNotFoundComponent} from './pages/page-not-found/page-not-found.component';
+import {LoginComponent} from './pages/auth/login/login.component';
+import {RegisterComponent} from './pages/auth/register/register.component';
+import {SiteLayoutComponent} from './layouts/site-layout/site-layout.component';
+import {ForgotPasswordComponent} from './pages/auth/forgot-password/forgot-password.component';
+import {ResetPasswordComponent} from './pages/auth/reset-password/reset-password.component';
+import {LoginGuard} from './auth/guard/login/login.guard';
+import {RegisterWelcomeComponent} from './pages/auth/register-welcome/register-welcome.component';
+import {RegisterGenderComponent} from './pages/auth/register-gender/register-gender.component';
+import {RegisterPersonalizationComponent} from './pages/auth/register-personalization/register-personalization.component';
+import {ProfileComponent} from "./pages/auth/profile/profile.component";
+import {DashboardComponent} from "./pages/dashboard/dashboard.component";
+import {CategoryComponent} from "./pages/category/category.component";
+import {CategoryGameListComponent} from "./pages/category-game-list/category-game-list.component";
+import {GameProfileComponent} from "./pages/game-profile/game-profile.component";
+import {GameProfileReviewComponent} from "./pages/game-profile-review/game-profile-review.component";
+import {PlaygroundComponent} from "./pages/playground/playground.component";
 
 
 const routes: Routes = [
+
+
+  // No Login Required
   {
     path : '',
-    component : HomepageComponent,
-    pathMatch : 'full'
+    component : SiteLayoutComponent,
+    children : [
+      { path : '', component : HomepageComponent},
+      { path : 'login', component : LoginComponent},
+      { path : 'register', component : RegisterComponent},
+      { path : 'forgot_password', component : ForgotPasswordComponent},
+      { path : 'reset_password', component : ResetPasswordComponent},
+      // might be guarded
+      { path : 'game/category', component : CategoryComponent},
+      { path : 'game/category/list', component : CategoryGameListComponent},
+      { path : 'game/profile', component : GameProfileComponent},
+      { path : 'game/profile/review', component : GameProfileReviewComponent},
+      { path : 'game/playground', component : PlaygroundComponent},
+    ]
   },
 
+  // Login Required
   {
-    path: '**',
-    component: PageNotFoundComponent
-  }
+    path : '',
+    component : SiteLayoutComponent,
+    // canActivate: [LoginGuard],
+    children : [
+      { path : 'register/welcome', component : RegisterWelcomeComponent},
+      { path : 'register/gender', component : RegisterGenderComponent},
+      { path : 'register/personalize', component : RegisterPersonalizationComponent},
+      { path : 'profile', component : ProfileComponent},
+      { path : 'dashboard', component : DashboardComponent},
+    ]
+  },
+
+  {path: '**', component: PageNotFoundComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,64 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomepageComponent } from './homepage/homepage.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { LoginComponent } from './pages/auth/login/login.component';
+import { RegisterComponent } from './pages/auth/register/register.component';
+import { FooterComponent } from './layouts/inc/footer/footer.component';
+import {SiteLayoutComponent} from './layouts/site-layout/site-layout.component';
+import { HeaderTemplateComponent } from './layouts/inc/header-template/header-template.component';
+import { ForgotPasswordComponent } from './pages/auth/forgot-password/forgot-password.component';
+import { AuthMenuComponent } from './pages/inc/auth-menu/auth-menu.component';
+import { ResetPasswordComponent } from './pages/auth/reset-password/reset-password.component';
+import { RegisterWelcomeComponent } from './pages/auth/register-welcome/register-welcome.component';
+
+import { RegisterGenderComponent } from './pages/auth/register-gender/register-gender.component';
+import { ProfileComponent } from './pages/auth/profile/profile.component';
+import { RegisterPersonalizationComponent } from './pages/auth/register-personalization/register-personalization.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { CategoryComponent } from './pages/category/category.component';
+import { CategoryGameListComponent } from './pages/category-game-list/category-game-list.component';
+import { GameProfileComponent } from './pages/game-profile/game-profile.component';
+import { GameProfileReviewComponent } from './pages/game-profile-review/game-profile-review.component';
+import { SearchbarComponent } from './pages/inc/searchbar/searchbar.component';
+import { PlaygroundComponent } from './pages/playground/playground.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomepageComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    LoginComponent,
+    RegisterComponent,
+    FooterComponent,
+    SiteLayoutComponent,
+    HeaderTemplateComponent,
+    ForgotPasswordComponent,
+    AuthMenuComponent,
+    ResetPasswordComponent,
+    RegisterWelcomeComponent,
+    RegisterGenderComponent,
+    ProfileComponent,
+    RegisterPersonalizationComponent,
+    DashboardComponent,
+    CategoryComponent,
+    CategoryGameListComponent,
+    GameProfileComponent,
+    GameProfileReviewComponent,
+    SearchbarComponent,
+    PlaygroundComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    // AuthModule,
+    AppRoutingModule,
   ],
   providers: [],
+  exports: [
+    AuthMenuComponent,
+    HeaderTemplateComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
